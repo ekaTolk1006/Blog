ea7209264a72ace311db37671a06b352c3beaca8
Ekaterina Tolkacheva <ekaterinaTolka4eva10-06@yandex.ru>
2021-12-03 14:59:46 +0200

    added routing

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+
+
+import { LoginPageComponent } from "./login-page/login-page.component";
+import { AdminLayoutComponent } from './shared/components/admin-layout/admin-layout.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { PostPageComponent } from "../post-page/post-page.component";
+
+
+@NgModule({
+    declarations: [
+        LoginPageComponent,
+        AdminLayoutComponent,
+        CreatePageComponent,
+        DashboardComponent,
+        EditPageComponent],
+
+        imports:[
+            ReactiveFormsModule,
+            FormsModule,
+            CommonModule,
+            RouterModule.forChild([
+                {path:'',component:AdminLayoutComponent},
+                {path:'login',component:LoginPageComponent},
+                {path:'dashboard', component:DashboardComponent},
+                {path:'post/:id/edit',component: PostPageComponent},
+                {path:'create',component:CreatePageComponent}
+            ])
+        
+        ],
+        exports:[
+            RouterModule
+        ]
+    })
+
+    export class AdminModule{
+
+    }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { PostPageComponent } from './post-page/post-page.component';
+import { MainLayoutComponent } from './shared/components/main-layout/main-layout.component';
+import { PostComponent } from './shared/components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    PostPageComponent,
+    MainLayoutComponent,
+    PostComponent,
+   
   ],
   imports: [
     BrowserModule,
